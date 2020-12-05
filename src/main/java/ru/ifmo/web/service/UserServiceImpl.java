c1020cf2ec0bc15586fa288aed21d8bdb81a06a0
Bobur Zakirov <bobur.octo@gmail.com>
2020-12-06 02:12:52 +0300

    Deleted unused import

--- src/main/java/ru/ifmo/web/service/UserServiceImpl.java
+++ src/main/java/ru/ifmo/web/service/UserServiceImpl.java
@@ -1,7 +1,6 @@
 package ru.ifmo.web.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
